5bcefdd1b80d6c6223d1bfd861b28b1f8625805b
Siddharth Pai <siddharth.pai@digifutura.com>
2016-10-24 12:03:30 +0530

    Video will play on click of play button or VR button

--- app.js
+++ app.js
@@ -6,13 +6,32 @@ $(document).ready(function() {
   var hideAframeScene = function(callback){
     $('.a-canvas').hide();
   };
+  var showPlayButton = function(callback){
+    $('#playButton').show();
+  }
+  var hidePlayButton = function(callback){
+    $('#playButton').hide();
+  }
+  var showLoader = function(callback){
+    $('#loader').show();
+  }
+  var hideLoader = function(callback){
+    $('#loader').hide();
+  }
 document.addEventListener('renderstart',function(){
+  showPlayButton();
+  hideLoader();
   document.querySelector('.a-enter-vr-button').addEventListener('click',function(event){
       var videoElement = document.querySelector('#video');
-
       videoElement.play();
+      hidePlayButton();
   });
+  document.querySelector('#playButton').addEventListener('click',function(event){
+    var videoElement = document.querySelector('#video');
+    videoElement.play();
+    hidePlayButton();
+  })
 });
 
-
+hidePlayButton();
 });
